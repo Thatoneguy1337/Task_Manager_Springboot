7785b2c38405d65823cec4d77a5df5afc3ef7f9e
Victor Guterres <Vgb1337@gmail.com>
2024-03-13 16:25:17 -0300

    feat: finished

--- task/src/main/java/task_manager/task/TaskEntity.java
+++ task/src/main/java/task_manager/task/TaskEntity.java
@@ -1,5 +1,60 @@
 package task_manager.task;
+import java.util.UUID;
 
+import javax.persistence.*;
+import lombok.Data;
+
+@Entity
 public class TaskEntity {
-    
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @NotEmpty
+    @Column(nullable = false)
+    private String title;
+
+    @NotEmpty
+    @Column(nullable = false)
+    private String status;
+
+    @NotEmpty
+    @Column(nullable = false)
+    private String description;
+
+    // Getters and setters
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
+
+
