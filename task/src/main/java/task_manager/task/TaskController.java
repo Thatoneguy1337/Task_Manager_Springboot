e3fd05d2ed4b04d1672e7914d6ebffcbebdd7559
Victor Guterres <Vgb1337@gmail.com>
2024-03-14 17:11:56 -0300

    feat: tarefa x finalizada

--- task/src/main/java/task_manager/task/TaskController.java
+++ task/src/main/java/task_manager/task/TaskController.java
@@ -1,5 +1,44 @@
 package task_manager.task;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.UUID;
 
+
+@RestController
+@RequestMapping("/tasks")
 public class TaskController {
-    
+
+    private final TaskServices taskService;
+
+    @Autowired
+    public TaskController(TaskServices taskService) {
+        this.taskService = taskService;
+    }
+
+    @PostMapping
+    public ResponseEntity<TaskEntity> createTask(@RequestBody TaskEntity task) {
+        TaskEntity createdTask = taskService.createTask(task);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdTask);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TaskEntity>> getAllTasks() {
+        List<TaskEntity> tasks = taskService.getAllTasks();
+        return ResponseEntity.ok(tasks);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TaskEntity> getTaskById(@PathVariable UUID id) {
+        return taskService.getTaskById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
 }
+
+
+
+
--- task/src/main/java/task_manager/task/TaskRepository.java
+++ task/src/main/java/task_manager/task/TaskRepository.java
@@ -1,5 +1,16 @@
 package task_manager.task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public class TaskRepository {
+
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface TaskRepository extends JpaRepository<TaskEntity, UUID> {
+    List<TaskEntity>findById( String id);
     
+    @SuppressWarnings("null")
+    List<TaskEntity>findAll();
 }
