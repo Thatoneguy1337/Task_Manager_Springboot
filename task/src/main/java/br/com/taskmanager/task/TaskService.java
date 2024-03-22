d6df7e094729ee0d5e920b9d901d218e521ecf88
Victor Guterres <Vgb1337@gmail.com>
2024-03-22 09:42:30 -0300

    Tofix: runserver

--- task/src/main/java/br/com/taskmanager/task/TaskService.java
+++ task/src/main/java/br/com/taskmanager/task/TaskService.java
@@ -1,21 +1,18 @@
 package br.com.taskmanager.task;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import br.com.taskmanager.task.dtos.TaskCreateDTO;
-
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import org.springframework.stereotype.Service;
 
+import br.com.taskmanager.task.dtos.TaskCreateDTO;
 
 @Service
 public class TaskService {
 
-    @Autowired
+    // @Autowired
     private TaskRepository taskRepository;
 
-
-      public TaskEntity createTask(TaskCreateDTO taskDto) {
+    public TaskEntity createTask(TaskCreateDTO taskDto) {
         TaskEntity task = new TaskEntity(taskDto.getTitle(), taskDto.getStatus(), taskDto.getDescription());
         return taskRepository.save(task);
     }
@@ -24,24 +21,26 @@ public class TaskService {
         return taskRepository.findAll();
     }
 
+    @SuppressWarnings("null")
     public Optional<TaskEntity> getTaskById(UUID id) {
         return taskRepository.findById(id);
     }
 
     public Optional<TaskEntity> updateTask(UUID id, TaskEntity updateTask) {
+        @SuppressWarnings("null")
         Optional<TaskEntity> optionalTask = taskRepository.findById(id);
-    
+
         TaskEntity existingTask = optionalTask.get();
         existingTask.setTitle(updateTask.getTitle());
         existingTask.setStatus(updateTask.getStatus());
         existingTask.setDescription(updateTask.getDescription());
-    
+
         return Optional.of(taskRepository.save(existingTask));
-        
+
     }
 
+    @SuppressWarnings("null")
     public void deleteTask(UUID id) {
         taskRepository.deleteById(id);
     }
 }
-    
